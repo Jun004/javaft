babe3e01e771e9922b7f60349f1353b96030cdb3
Jun004 <119747200+Jun004@users.noreply.github.com>
2023-06-03 00:52:06 +0900

    Add files via upload

---
+++ SelfIntroduction.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 氏名
+		String name = "山田　太郎";
+		// 年齢
+		int age = 26;
+		// 趣味
+		String hobby = "釣り";
+		
+		
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
